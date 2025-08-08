ec25f4fd365f8955df647dec03d559b08808d984
giancarlo-garbagnati <giancarlo.garbagnati@gmail.com>
2025-08-08 16:39:32 -0700

    gitignore and requirements update

--- scraping/scrape_questions.py
+++ scraping/scrape_questions.py
@@ -1,1 +1,19 @@
 # android studio
+
+
+"""
+source
+test_no
+q_no
+q_type
+solution
+num_answers
+answer1
+answer2
+answer3
+answer4
+answer5
+
+source_name,source_url,test_no,q_no,q_type,question_text,options,correct_options,explanation,topic,tags,language
+
+"""
